e622ca88586dbd81fa17590b22d399fb67da4511
Wicky2001 <pmwickramaratna@gmail.com>
2025-03-05 22:17:10 +0530

    now the code goes to hot news extract all news article topics and then visit to them again and extrct all news headings

--- adaderana_scraper/adaderana_scraper/spiders/adaderana.py
+++ adaderana_scraper/adaderana_scraper/spiders/adaderana.py
@@ -13,6 +13,31 @@
 #         yield {"link": links}
 
 
+# import scrapy
+# from scrapy.spiders import CrawlSpider, Rule
+# from scrapy.linkextractors import LinkExtractor
+
+# class AdaderanaSpider(CrawlSpider):
+#     name = 'adaderana'
+#     allowed_domains = ["sinhala.adaderana.lk"]
+#     start_urls = ["https://sinhala.adaderana.lk/"]
+
+#     # Define the rules for crawling
+#     rules = (
+#         Rule(LinkExtractor(allow='sinhala-hot-news'), callback='parse_items'),
+#     )
+
+#     # Define the callback method that processes each extracted link
+#     def parse_items(self, response):
+#         # Extract the links from the page
+#         links = response.css("a::attr(href)").getall()
+#         # Yield the extracted links
+#         for link in links:
+#             yield {"link": link}
+
+
+
+
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
@@ -24,17 +49,25 @@ class AdaderanaSpider(CrawlSpider):
 
     # Define the rules for crawling
     rules = (
-        Rule(LinkExtractor(allow='sinhala-hot-news'), callback='parse_items'),
+        Rule(LinkExtractor(allow='sinhala-hot-news'), callback='parse_article_links'),
     )
 
-    # Define the callback method that processes each extracted link
-    def parse_items(self, response):
-        # Extract the links from the page
+    # Callback to handle the first page of links with "sinhala-hot-news"
+    def parse_article_links(self, response):
+        # Extract all article links from the current page that contain "news"
         links = response.css("a::attr(href)").getall()
-        # Yield the extracted links
         for link in links:
-            yield {"link": link}
+            # Only follow links that contain "news"
+            if 'news' in link:
+                full_link = response.urljoin(link)
+                print("This is the full link = ",full_link)
+                yield scrapy.Request(full_link, callback=self.parse_news)
 
-
-
-
+    # Callback to parse the news article page and extract the title
+    def parse_news(self, response):
+        # Extract the news title from the page
+        title = response.css('h1::text').get()  # You may need to adjust this selector based on the structure of the page
+        yield {
+            'title': title,
+            'url': response.url
+        }
