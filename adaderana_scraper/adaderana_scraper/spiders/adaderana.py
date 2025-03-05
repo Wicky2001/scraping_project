9633af6a781767c46eaae227275adc0b6731e6cb
Wicky2001 <pmwickramaratna@gmail.com>
2025-03-06 00:19:56 +0530

    add if clause to remove null titles

--- adaderana_scraper/adaderana_scraper/spiders/adaderana.py
+++ adaderana_scraper/adaderana_scraper/spiders/adaderana.py
@@ -65,14 +65,20 @@ class AdaderanaSpider(CrawlSpider):
 
     # Callback to parse the news article page and extract the title
     def parse_news(self, response):
-        # Extract the news title from the page
-            title = response.css('article.news h1.news-heading::text').get()
-            body = response.css('article.news div.news-content p::text').get()
+           
+                  # Extract the news title from the page
+                title = response.css('article.news h1.news-heading::text').get()
+                body = response.css('article.news div.news-content p::text').get()
 
-            yield {
-            'title': title,
-            'body': body
-        }
+                if title:
+                    yield {
+                    'title': title,
+                    'body': body
+                    }
+                    
+
+             
+       
 
 
           # You may need to adjust this selector based on the structure of the page
